5bef6e96d2546ab917f49ccdb19fdd591d197220
EAMP2000 <98430523+EAMP2000@users.noreply.github.com>
2022-08-09 14:37:59 -0600

    Carpeta Consumo G6-20

---
+++ JS/Pasajero.js
@@ -0,0 +1,31 @@
+var urlPasajeros = 'http://20.216.41.245:90/G6_20/Pasajero/controller/pasajero_vuelo.php?opc=GetPasajeros';
+
+$(document).ready(function(){
+   CargarPasajeros();
+});
+
+function CargarPasajeros(){
+    $.ajax({
+        url: urlPasajeros,
+        type: 'GET',
+        datatype: 'JSON',
+        success: function(reponse){
+            var MiItems = reponse;
+            var Valores = "";
+
+           for(i=0; i<MisItems.length; i++)
+            Valores+=  `<tr>` + 
+            '<td>' + MiItems[i].CodigoPasajero +`</td>`+
+            '<td>' + MiItems[i].Nombres +`</td>`+
+            '<td>' + MiItems[i].Apellidos +`</td>`+
+            '<td>' + MiItems[i].FechaDeRegistro +`</td>`+
+            '<td>' + MiItems[i].Nacionalidad +`</td>`+
+            '<td>' + MiItems[i].NumeroTelefonico +`</td>`+
+            '<td>' + MiItems[i].Email +`</td>`+
+            '<tr>' 
+
+            $('#DataPasajeros').html(Valores);
+        }
+
+    });
+}
